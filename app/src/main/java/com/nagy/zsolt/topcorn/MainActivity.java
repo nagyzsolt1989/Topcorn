60192e683d3082cb20ac7f46e976426eb3216961
Zsolti <nagy.zsolt.1989@gmail.com>
2018-04-10 21:41:41 +0200

    Add watchlist navigation drawer

--- app/src/main/java/com/nagy/zsolt/topcorn/MainActivity.java
+++ app/src/main/java/com/nagy/zsolt/topcorn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nagy.zsolt.topcorn;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,20 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.nagy.zsolt.topcorn.data.FavouritesDBHelper;
+import com.nagy.zsolt.topcorn.data.FavourtiesContract;
+import com.nagy.zsolt.topcorn.data.WatchlistContract;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public static SQLiteDatabase mDb;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    String[] mWatchist;
     Context mContext;
 
     @Override
@@ -42,10 +57,32 @@ public class MainActivity extends AppCompatActivity {
 
 //        gridView = (GridView) findViewById(R.id.gridview);
 
-        mContext = getApplicationContext();
-
         FavouritesDBHelper dbHelper = new FavouritesDBHelper(this);
         mDb = dbHelper.getWritableDatabase();
+
+        mContext = getApplicationContext();
+        mWatchist = getWatchlist();
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Watchlist")
+                )
+                .build();
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(false)
+                .withAccountHeader(headerResult)
+                .build();
+
+        for (int i = 0; i < mWatchist.length; i++) {
+            System.out.println("Ezek a favouritek:" + mWatchist[i]);
+            System.out.println(mWatchist.length);
+            result.addItem(new PrimaryDrawerItem().withName(mWatchist[i]).withIdentifier(i).withSelectable(false));
+        }
     }
 
     /**
@@ -57,6 +94,27 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new TopRatedMovies(), "Top Rated");
         adapter.addFragment(new FavouriteMovies(), "Favourites");
         viewPager.setAdapter(adapter);
+    }
+
+    /**
+     * Select all movies on Watchlist
+     */
+    public String[] getWatchlist() {
+
+
+        String selectQuery = "SELECT " + WatchlistContract.WatchlistEntry.COLUMN_MOVIE_TITLE + " FROM " + WatchlistContract.WatchlistEntry.TABLE_NAME;
+        Cursor cursor = mDb.rawQuery(selectQuery, null);
+        String[] data = null;
+        ArrayList<String> itemIds = new ArrayList<String>();
+
+        while (cursor.moveToNext()) {
+            String itemId = cursor.getString(cursor.getColumnIndex(WatchlistContract.WatchlistEntry.COLUMN_MOVIE_TITLE));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+        data = itemIds.toArray(new String[0]);
+        return data;
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
